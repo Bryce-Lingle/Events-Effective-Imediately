b277e3669f895d1a080bc739c1c95c965eb827ba
BryceLingle <brycealingle@gmail.com>
2023-05-29 23:46:29 -0600

    date is included on the page

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,11 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+var todaysDate = document.getElementById('currentDay')
+
+var todaysDate = dayjs();
+$('#currentDay').text(todaysDate.format('dddd, MMMM D, YYYY'))
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
