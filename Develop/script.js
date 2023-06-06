04fe38222618ba9f3c45cbd28aadf6a0d68f1322
BryceLingle <brycealingle@gmail.com>
2023-06-06 00:19:58 -0600

    ONE CHANGE AT A TIME

--- Develop/script.js
+++ Develop/script.js
@@ -3,7 +3,10 @@
 // in the html.
 var todaysDate = document.getElementById('currentDay')
 var saveBtn = $('.saveBtn')
-var plan = document.getElementsByClassName('description')
+var plan = $('#text-area')
+var blockHour = $('hour-9 hour-10 hour-11 hour-12 hour-1 hour-2 hour-3 hour-4 hour-5')
+var currentHour = dayjs().format()
+
 
 
 //Display todays date
@@ -12,7 +15,12 @@ $('#currentDay').text(todaysDate.format('dddd, MMMM D, YYYY'))
 
 
 $(function () {
-  
+ //Button command so that the click effect works and saves to local storage.... STILL GETTING UNDEFINED 
+  saveBtn.on('click', function() {
+    var plan = $('#text-area').val();
+    localStorage.setItem('',plan);
+    console.log(plan.val)
+  })
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -20,6 +28,19 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
+
+  if (blockHour < currentHour) {
+    $(this).removeClass("present future") .addClass('past');
+  } else if (blockHour === currentHour){
+    $(this).removeClass('future past').addClass('present');
+  }else if (blockHour > currentHour) {
+    $(this).removeClass('past present').addClass('future');
+  }
+  
+
+
+//Button command so that the click effect works
+
 
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
@@ -38,10 +59,9 @@ $(function () {
 
 });
 
-//Button command so that the click effect works
-saveBtn.on('click', function() {
-  console.log ("button is clicked")
-})
+
+
+
 
 
   
