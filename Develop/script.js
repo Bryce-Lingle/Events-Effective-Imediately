c210f58bb6a08e571c06b4101a960092a4b650df
BryceLingle <brycealingle@gmail.com>
2023-06-05 23:11:17 -0600

    button works

--- Develop/script.js
+++ Develop/script.js
@@ -2,11 +2,17 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 var todaysDate = document.getElementById('currentDay')
+var saveBtn = $('.saveBtn')
+var plan = document.getElementsByClassName('description')
 
+
+//Display todays date
 var todaysDate = dayjs();
 $('#currentDay').text(todaysDate.format('dddd, MMMM D, YYYY'))
 
+
 $(function () {
+  
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -14,15 +20,34 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
+
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
   //
+
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
+
   //
   // TODO: Add code to display the current date in the header of the page.
+
+
 });
+
+//Button command so that the click effect works
+saveBtn.on('click', function() {
+  console.log ("button is clicked")
+})
+
+
+  
+
+
+
+
+  
+  
