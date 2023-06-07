558f7f0454c39ca3881431a632c163bcafab04a9
BryceLingle <brycealingle@gmail.com>
2023-06-07 10:04:37 -0600

    local stoarge for all times

--- Develop/script.js
+++ Develop/script.js
@@ -3,8 +3,7 @@
 // in the html.
 var todaysDate = document.getElementById('currentDay')
 var saveBtn = $('.saveBtn')
-var plan = $('#text-area')
-//var blockHour; 
+var plan = $('#text-area') 
 var currentHour = dayjs().format()
 
 
@@ -20,22 +19,7 @@ $(function () {
     localStorage.setItem(key,plan);
     console.log(plan)
   })
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
 
-
-
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //For day js - dayjs().hour()
 $('.time-block').each(function (){
   var blockHour = parseInt($(this).attr('id').split('-')[1]);
   var currentTime = dayjs().hour(); 
@@ -59,12 +43,25 @@ $('.time-block').each(function (){
 
 $('#hour-9 #text-area').val(localStorage.getItem('hour-9'))
 
+$('#hour-10 #text-area').val(localStorage.getItem('hour-10'))
+
+$('#hour-11 #text-area').val(localStorage.getItem('hour-11'))
+
+$('#hour-12 #text-area').val(localStorage.getItem('hour-12'))
+
+$('#hour-13 #text-area').val(localStorage.getItem('hour-13'))
+
+$('#hour-14 #text-area').val(localStorage.getItem('hour-14'))
+
+$('#hour-15 #text-area').val(localStorage.getItem('hour-15'))
+
+$('#hour-16 #text-area').val(localStorage.getItem('hour-16'))
+
+$('#hour-17 #text-area').val(localStorage.getItem('hour-17'))
+
+
 });
 
-//Questions:
-//M<y save button is only working for the first button. How do I tie 'this' into it to make each button work?
-//I have currentTime set for 24 hour clock. How do I compare it to the blockHour which is representative of the div id's "hour-X"?
-//How do I get the elements from local storage in order to pull up my to do's when the page is loaded? 
 
 
 
